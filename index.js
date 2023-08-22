c66037a0e6cb588c73d59ce6ef73ce3ad3a4a816
harsh <harshjaiswal9450@gmail.com>
2023-08-22 20:10:51 +0530

    6

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,   'home.html')); //
+    res.sendFile(path.join(__dirname,   'Home.html')); //
 });
 
 
