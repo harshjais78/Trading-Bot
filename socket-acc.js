8461e3bd01262897d736397615494e42a6acc7eb
harsh <harshjaiswal9450@gmail.com>
2023-12-16 22:43:06 +0530

    feature paused

--- socket-acc.js
+++ socket-acc.js
@@ -31,69 +31,69 @@ socket.on("connect", () => {
 
 
 
-socket.on("order-update", async (response) => {
-  console.log('order-update');
-  try{
-  let sudden_fallJson=await getStoredJson(CONSTANT.suddenFall);
-  let short_termJson=await getStoredJson(CONSTANT.shortTerm);
+// socket.on("order-update", async (response) => {
+//   console.log('order-update');
+//   try{
+//   let sudden_fallJson=await getStoredJson(CONSTANT.suddenFall);
+//   let short_termJson=await getStoredJson(CONSTANT.shortTerm);
 
-  let a=JSON.parse(response.data);
+//   let a=JSON.parse(response.data);
 
-  for(let obj of a) {
-    let client_order_id=obj.client_order_id;
+//   for(let obj of a) {
+//     let client_order_id=obj.client_order_id;
 
-    if( obj.status =='cancelled') continue;
+//     if( obj.status =='cancelled') continue;
   
-    if((client_order_id== null || client_order_id==undefined) ){
-     await cancelOrder(obj.id);
-      await placeSameOrder(obj,short_termJson);  //Note: if order is placed from app then same order will be regenerated with client_order_id 
-      // and if side= buy then coin will be sold at short-term profit
-      continue; 
-    }
+//     if((client_order_id== null || client_order_id==undefined) ){
+//      await cancelOrder(obj.id);
+//       await placeSameOrder(obj,short_termJson);  //Note: if order is placed from app then same order will be regenerated with client_order_id 
+//       // and if side= buy then coin will be sold at short-term profit
+//       continue; 
+//     }
 
-    if(obj.side=='sell'){ // coin is sold, Notify
-  if(sudden_fallJson.order_id.buy.includes(client_order_id)){
-    if(obj.status=='filled' ){
-      sendEmail(`You have successfully sold ${obj.target_currency_name} with the profit of 6% probably`);
-      moveOrderIdToSell(client_order_id,CONSTANT.suddenFall);
-    }
-  }else{
-    if(client_order_id.endsWith("loss"))
-      sendEmail(`You earned loss of more than 13% on ${obj.target_currency_name}`); 
-   else if(obj.status=='filled' ){
-      sendEmail(`You have successfully sold ${obj.target_currency_name} with the profit of 4% probably`); 
-      moveOrderIdToSell(client_order_id,CONSTANT.shortTerm);
+//     if(obj.side=='sell'){ // coin is sold, Notify
+//   if(sudden_fallJson.order_id.buy.includes(client_order_id)){
+//     if(obj.status=='filled' ){
+//       sendEmail(`You have successfully sold ${obj.target_currency_name} with the profit of 6% probably`);
+//       moveOrderIdToSell(client_order_id,CONSTANT.suddenFall);
+//     }
+//   }else{
+//     if(client_order_id.endsWith("loss"))
+//       sendEmail(`You earned loss of more than 13% on ${obj.target_currency_name}`); 
+//    else if(obj.status=='filled' ){
+//       sendEmail(`You have successfully sold ${obj.target_currency_name} with the profit of 4% probably`); 
+//       moveOrderIdToSell(client_order_id,CONSTANT.shortTerm);
       
-    }
-  }
+//     }
+//   }
 
-    }else{  // buy coin is triggered from some where but not yet filled
-      if(sudden_fallJson.order_id.buy.includes(client_order_id)){
-        if(obj.status=='filled' ){    // buy coin is filled
-          let quant=obj.total_quantity;
-          let old_ppu=obj.price_per_unit;
-          let new_ppu=old_ppu+(old_ppu*6)/100;
-          let client_id=generateClientOrderId(obj.market);
-          sellCoins(obj.market,quant,new_ppu,client_id);
-          sudden_fallJson.order_id.sell.push(client_id);
-          saveResults(sudden_fallJson,CONSTANT.suddenFall);
-        }
-      }else{  //order was set for short-term trade
-        if(obj.status=='filled' ){
-          let quant=obj.total_quantity;
-          let old_ppu=obj.price_per_unit;
-          let new_ppu=old_ppu+(old_ppu*4)/100;
-          let client_id=generateClientOrderId(obj.market);
-          sellCoins(obj.market,quant,new_ppu,client_id);
-          short_termJson.order_id.sell.push(client_id);
-          saveResults(short_termJson,CONSTANT.shortTerm);
-        }
-      }
-    }
+//     }else{  // buy coin is triggered from some where but not yet filled
+//       if(sudden_fallJson.order_id.buy.includes(client_order_id)){
+//         if(obj.status=='filled' ){    // buy coin is filled
+//           let quant=obj.total_quantity;
+//           let old_ppu=obj.price_per_unit;
+//           let new_ppu=old_ppu+(old_ppu*6)/100;
+//           let client_id=generateClientOrderId(obj.market);
+//           sellCoins(obj.market,quant,new_ppu,client_id);
+//           sudden_fallJson.order_id.sell.push(client_id);
+//           saveResults(sudden_fallJson,CONSTANT.suddenFall);
+//         }
+//       }else{  //order was set for short-term trade
+//         if(obj.status=='filled' ){
+//           let quant=obj.total_quantity;
+//           let old_ppu=obj.price_per_unit;
+//           let new_ppu=old_ppu+(old_ppu*4)/100;
+//           let client_id=generateClientOrderId(obj.market);
+//           sellCoins(obj.market,quant,new_ppu,client_id);
+//           short_termJson.order_id.sell.push(client_id);
+//           saveResults(short_termJson,CONSTANT.shortTerm);
+//         }
+//       }
+//     }
 
-  }
-}catch(err){console.error(err);}
-});
+//   }
+// }catch(err){console.error(err);}
+// });
 
 // In order to leave a channel
 // socket.emit('leave', {
