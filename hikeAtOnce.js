4436046ef2e5f89980001794f9723a77eec63f6e
harsh <harshjaiswal9450@gmail.com>
2024-02-01 15:01:19 +0530

    changed selling algo for spike greedy sell

--- hikeAtOnce.js
+++ hikeAtOnce.js
@@ -269,81 +269,87 @@ export async function spikeGreedySell(coinsWithHike, id){
             sendLogs(`${prefix(_id)} Enough time given, Selling, cnt: ${cnt} Earned, ${percentageEarned}`);
             
         }
-          if ( percentageEarned >= 3 || percentageEarned < maxLossAccepted ){  // if price is bw -8 to 3 then do nothing, hope coin to inc more than 3%
 
-            if(percentageEarned >= 7)
-            moreThan3cnt++;
+        if(percentageEarned >= 3.6 || percentageEarned <maxLossAccepted){
+          beGreedy(coinsWithHike,_id,0);
+        }
+          sendLogs(`${prefix(_id)} inside spikeGreedySell: price: ${currentPrice}  percentageEarned: ${percentageEarned}`); 
 
-            if(percentageEarned >= 12)
-             moreThan12cnt++;
+        //   if ( percentageEarned >= 3 || percentageEarned < maxLossAccepted ){  // if price is bw -8 to 3 then do nothing, hope coin to inc more than 3%
 
-             if(moreThan12cnt >= 3)
-               maxLossAccepted = 3;
+        //     if(percentageEarned >= 7)
+        //     moreThan3cnt++;
 
-            if(isMoreThan3){
-              if(lastcnt + 2 <= cnt ){ // after 2 min
-                if(currentPrice < lastPrice){
-                  maxLossAccepted = -5;
-                } 
-                if(currentPrice < lastPrice){
-                    if(profitArr.length > 3 && (profitArr[3]- profitArr[1] < -15 ||  profitArr[3]- profitArr[0] < -15 )){ // decreased suddenly.
-                        sendLogs(`${prefix(_id)} seems to be -ve wick,so skipping selling part. Sum of last 3 percEarned: ${profitArr[0]} ${profitArr[1]}  ${profitArr[2]}  ${profitArr[3]}`);
-                    }else{
-                sendLogs(`${prefix(_id)} Being Greedy: perc. Earned: ${percentageEarned.toFixed(3)}% last Price: ${lastPrice} currentPrice: ${currentPrice}`);
-                // beGreedy(coinsWithHike,_id,-0.5);
-                  clearInterval(intervalId);
-                }
-            }else{
-                is1minInc = true;
-            }
-                lastPrice = currentPrice;
-                lastcnt = cnt;
-                sendLogs(`${prefix(_id)} After 2 min perc. Earned: ${percentageEarned.toFixed(3)}% last Price: ${lastPrice} `);
-              }
+        //     if(percentageEarned >= 12)
+        //      moreThan12cnt++;
 
-              profitArr.push(percentageEarned);
-              if(profitArr.length >3){
-              profitArr=profitArr.slice(-4);
-              sendLogs(`${prefix(_id)} profitArr: ${profitArr.toString()}`);
-              if( profitArr[2] -profitArr[0] >= 15 || profitArr[3] - profitArr[0] >= 15){   // increased suddenly
-                sendLogs(`${prefix(_id)} seems to be wick, so selling. Sum of last 3 percEarned: ${profitArr[2] - profitArr[0]}`);
-                beGreedy(coinsWithHike,_id,-1);
-            }else if(profitArr.length > 3 && (profitArr[3]- profitArr[1] < -15 ||  profitArr[3]- profitArr[0] < -15 )){ // decreased suddenly.
-                sendLogs(`${prefix(_id)} seems to be -ve wick,so skipping selling part. Sum of last 3 percEarned: ${profitArr[0]} ${profitArr[1]}  ${profitArr[2]}  ${profitArr[3]}`);
-                return;
-            }
-            }
-            if(is1minInc){
-                sendLogs(`${prefix(_id)} 1min candle is still increasing, so skip selling. Earned ${percentageEarned}`);
-                return; // increase as far as possible.
-            }
-            }
+        //      if(moreThan12cnt >= 3)
+        //        maxLossAccepted = 3;
 
-            if(moreThan3cnt >= 4){
-              isMoreThan3 = true;
-              lastPrice = currentPrice;
-              sendLogs(`${prefix(_id)} more than 3 = true: ${percentageEarned.toFixed(3)}`);
-            }
+        //     if(isMoreThan3){
+        //       if(lastcnt + 2 <= cnt ){ // after 2 min
+        //         if(currentPrice < lastPrice){
+        //           maxLossAccepted = -5;
+        //         } 
+        //         if(currentPrice < lastPrice){
+        //             if(profitArr.length > 3 && (profitArr[3]- profitArr[1] < -15 ||  profitArr[3]- profitArr[0] < -15 )){ // decreased suddenly.
+        //                 sendLogs(`${prefix(_id)} seems to be -ve wick,so skipping selling part. Sum of last 3 percEarned: ${profitArr[0]} ${profitArr[1]}  ${profitArr[2]}  ${profitArr[3]}`);
+        //             }else{
+        //         sendLogs(`${prefix(_id)} Being Greedy: perc. Earned: ${percentageEarned.toFixed(3)}% last Price: ${lastPrice} currentPrice: ${currentPrice}`);
+        //         // beGreedy(coinsWithHike,_id,-0.5);
+        //           clearInterval(intervalId);
+        //         }
+        //     }else{
+        //         is1minInc = true;
+        //     }
+        //         lastPrice = currentPrice;
+        //         lastcnt = cnt;
+        //         sendLogs(`${prefix(_id)} After 2 min perc. Earned: ${percentageEarned.toFixed(3)}% last Price: ${lastPrice} `);
+        //       }
 
-            if(percentageEarned >= targetProfit || percentageEarned < maxLossAccepted){
+        //       profitArr.push(percentageEarned);
+        //       if(profitArr.length >3){
+        //       profitArr=profitArr.slice(-4);
+        //       sendLogs(`${prefix(_id)} profitArr: ${profitArr.toString()}`);
+        //       if( profitArr[2] -profitArr[0] >= 15 || profitArr[3] - profitArr[0] >= 15){   // increased suddenly
+        //         sendLogs(`${prefix(_id)} seems to be wick, so selling. Sum of last 3 percEarned: ${profitArr[2] - profitArr[0]}`);
+        //         beGreedy(coinsWithHike,_id,-1);
+        //     }else if(profitArr.length > 3 && (profitArr[3]- profitArr[1] < -15 ||  profitArr[3]- profitArr[0] < -15 )){ // decreased suddenly.
+        //         sendLogs(`${prefix(_id)} seems to be -ve wick,so skipping selling part. Sum of last 3 percEarned: ${profitArr[0]} ${profitArr[1]}  ${profitArr[2]}  ${profitArr[3]}`);
+        //         return;
+        //     }
+        //     }
+        //     if(is1minInc){
+        //         sendLogs(`${prefix(_id)} 1min candle is still increasing, so skip selling. Earned ${percentageEarned}`);
+        //         return; // increase as far as possible.
+        //     }
+        //     }
+
+        //     if(moreThan3cnt >= 4){
+        //       isMoreThan3 = true;
+        //       lastPrice = currentPrice;
+        //       sendLogs(`${prefix(_id)} more than 3 = true: ${percentageEarned.toFixed(3)}`);
+        //     }
+
+        //     if(percentageEarned >= targetProfit || percentageEarned < maxLossAccepted){
           
-              if( percentageEarned < maxLossAccepted){
-                // sell if waited for appr. 2hrs bw -8 to -11 or it is more than 30 min or too much of loss
-              sell(intervalId,_id,symbol,boughtPrice,currentPrice," ",percentageEarned,cntLoss,cntLossRestore,cnt)
-            }else{
-              sendLogs(`${prefix(_id)} inside greedy sell: targetProfit: ${targetProfit} percentageEarned: ${percentageEarned}`); 
-                beGreedy(coinsWithHike,_id,0);
-              clearInterval(intervalId);
-             }
+        //       if( percentageEarned < maxLossAccepted){
+        //         // sell if waited for appr. 2hrs bw -8 to -11 or it is more than 30 min or too much of loss
+        //       sell(intervalId,_id,symbol,boughtPrice,currentPrice," ",percentageEarned,cntLoss,cntLossRestore,cnt)
+        //     }else{
+        //       sendLogs(`${prefix(_id)} inside greedy sell: targetProfit: ${targetProfit} percentageEarned: ${percentageEarned}`); 
+        //         beGreedy(coinsWithHike,_id,0);
+        //       clearInterval(intervalId);
+        //      }
 
-            }
+        //     }
          
-            sendLogs(`${prefix(_id)} trying to sell coin: ${symbol} with (>3% or <-8%) cnt: ${cnt} current price: ${currentPrice} Percentage Earned: ${percentageEarned.toFixed(3)} targetProfit: ${targetProfit}`);
-          }
-         else{
-          sendLogs(`${prefix(_id)} trying to sell coin: ${symbol} cnt: ${cnt}, pending Percentage Earned: ${percentageEarned.toFixed(3)} cntLossRestore: ${cntLossRestore}`);
-            moreThan3cnt = 0;
-        } 
+        //     sendLogs(`${prefix(_id)} trying to sell coin: ${symbol} with (>3% or <-8%) cnt: ${cnt} current price: ${currentPrice} Percentage Earned: ${percentageEarned.toFixed(3)} targetProfit: ${targetProfit}`);
+        //   }
+        //  else{
+        //   sendLogs(`${prefix(_id)} trying to sell coin: ${symbol} cnt: ${cnt}, pending Percentage Earned: ${percentageEarned.toFixed(3)} cntLossRestore: ${cntLossRestore}`);
+        //     moreThan3cnt = 0;
+        // } 
         
       }
     } catch (error) {
