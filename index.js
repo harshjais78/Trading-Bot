79fd7318e36823d17c60d7a3c45e8364c047da08
harsh <harshjaiswal9450@gmail.com>
2024-02-19 00:10:33 +0530

    3

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import { flatCoins, getStoredJson } from './util.js';
 import './schedule.js'
 import './socket-acc.js'
+import bodyParser from 'body-parser';
 import * as CONSTANT from './Constant.js'
  
 
@@ -14,6 +15,7 @@ const __dirname = path.dirname(__filename);
 let port=process.env.PORT || 8080;
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public'))); 
 
@@ -23,15 +25,18 @@ app.get('/', (req, res) => {
 });
 
 app.post('/deletion', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
+    const { username, password } = {username:"harsh", password:"1234"};
+  
     // Check if username and password are provided
     if (!username || !password) {
-        return res.status(400).send('Username and password are required.');
+      return res.status(400).json({ message: 'Username and password are required.' });
     }
-    res.status(200).send('Your account will be deleted in 90 days.');
-});
+  
+    // Perform deletion logic here (e.g., delete user account)
+    // For demonstration purposes, let's assume deletion was successful
+    res.status(200).json({ message: 'Your account will be deleted in 90 days.' });
+  });
+  
 
 app.get('/check', async (req, res) => {
     try {
