e324537d9a61f446fb0cefc096dc11a83e85e795
harshjais78 <harshjais78@gmail.com>
2025-08-27 20:15:49 +0530

    add log to check repetetive service call

--- algo2k25/index.js
+++ algo2k25/index.js
@@ -163,6 +163,7 @@ export async function monitorPrices() {
 export async function checkReboundCandidates() {
     try {
         if (reboundWatchlist.length == 0) return;
+        sendLogs(`reboundWatchlist: ${reboundWatchlist} ${reboundWatchlist.length}`)
         const  latestData  = await fetchAllPrices()
         const now = Date.now();
 
@@ -201,6 +202,7 @@ export async function checkReboundCandidates() {
 export async function manageBoughtCoins() {
     try {
         if (boughtCoins.length == 0) return;
+        sendLogs(`boughtCoins: ${boughtCoins} ${boughtCoins.length}`)
         const prices = await fetchAllPrices();
 
         for (const coin of prices) {
